c485d3f9934414b0232d3a01c34d9424794ca489
SeongJaeSong <tjdkskgnal61@gmail.com>
2020-01-09 11:11:27 -0800

    Create Google-Sign-Up form

--- server/src/database.js
+++ server/src/database.js
@@ -1,9 +1,9 @@
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
 const createStore = () => {
   const sequelize = new Sequelize({
-    dialect: 'sqlite',
-    storage: './database.sqlite',
+    dialect: "sqlite",
+    storage: "./database.sqlite"
   });
 
   sequelize.authenticate().then(() => {
@@ -21,120 +21,130 @@ const createStore = () => {
   class UserParticipatedEvent extends Model {}
 
   User.init({
-        id: {
-          type: Sequelize.INTEGER,
-          primaryKey: true,
-          autoIncrement: true,
-        },
-        createdAt: Sequelize.DATE,
-        firstName: {type: Sequelize.STRING, allowNull: false},
-        lastName: {type: Sequelize.STRING, allowNull: false},
-        googleId: Sequelize.STRING,
-        facebookId: Sequelize.STRING,
-        profileImgUrl: Sequelize.STRING,
-        email: {
-          type: Sequelize.STRING, allowNull: false,
-        },
-        token: Sequelize.STRING,
-        birthday: Sequelize.DATE,
-        city: Sequelize.STRING,
-        state: Sequelize.STRING,
-        phone: Sequelize.STRING,
-      },
-      {
-        sequelize,
-        modelName: 'user',
-      });
-
-  Event.init({
     id: {
       type: Sequelize.INTEGER,
       primaryKey: true,
       autoIncrement: true,
     },
-    createdAt: Sequelize.DATE,
-    updatedAt: Sequelize.DATE,
-    hostId: {
-      type: Sequelize.INTEGER,
-      references: {
-        model: User,
-        key: 'id',
+      createdAt: Sequelize.DATE,
+      firstName: {type: Sequelize.STRING, allowNull: false},
+      lastName: {type: Sequelize.STRING, allowNull: false},
+      googleId: Sequelize.STRING,
+      facebookId: Sequelize.STRING,
+      profileImgUrl: Sequelize.STRING,
+      email: {
+        type: Sequelize.STRING, allowNull: false,
       },
+      token: Sequelize.STRING,
+      birthday: Sequelize.DATE,
+      city: Sequelize.STRING,
+      state: Sequelize.STRING,
+      phone: Sequelize.STRING,
     },
-    title: Sequelize.STRING,
-    description: Sequelize.STRING,
-    price: Sequelize.FLOAT,
-    maxParticipants: Sequelize.INTEGER,
-  }, {
-    sequelize,
-    modelName: 'event',
-  });
+    {
+      sequelize,
+      modelName: "user"
+    }
+  );
+
+  Event.init(
+    {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      createdAt: Sequelize.DATE,
+      updatedAt: Sequelize.DATE,
+      hostId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: User,
+          key: "id"
+        }
+      },
+      title: Sequelize.STRING,
+      description: Sequelize.STRING,
+      price: Sequelize.FLOAT,
+      maxParticipants: Sequelize.INTEGER
+    },
+    {
+      sequelize,
+      modelName: "event"
+    }
+  );
 
   // Every event can have multiple tags.
   // Tags are predefined by ours(developers)
   // and used by the event host to categorize his event.
   // So we are able to categorize events by tags.
-  Tag.init({
-    id: {
-      type: Sequelize.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    name: {
-      type: Sequelize.STRING,
-      allowNull: false,
-    },
-    eventId: {
-      type: Sequelize.INTEGER,
-      references: {
-        model: Event,
-        key: 'id',
+  Tag.init(
+    {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
       },
-    },
-  }, {
-    sequelize,
-    modelName: 'tag',
-  });
-
-  Schedule.init({
-    id: {
-      type: Sequelize.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    start: Sequelize.DATE,
-    end: Sequelize.DATE,
-    locationLatitude: Sequelize.FLOAT,
-    locationLongitude: Sequelize.FLOAT,
-    eventId: {
-      type: Sequelize.INTEGER,
-      references: {
-        model: Event,
-        key: 'id',
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
       },
+      eventId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: Event,
+          key: "id"
+        }
+      }
     },
-    // TODO(arin-kwak): Find better way to deal with repetition.
-    //  Maybe we can reference calendar apps to improve this.
-    //  This method can't deal with 'evey other tuesday'
-    // If 'repeat' is null, this schedule is not repeated.
-    repeat: Sequelize.ENUM('week', 'month', 'year'),
-  }, {
-    sequelize,
-    modelName: 'schedule',
-    timestamps: false,
-  });
+    {
+      sequelize,
+      modelName: "tag"
+    }
+  );
 
-  UserParticipatedEvent.init({
-        userId: {
-          type: Sequelize.INTEGER,
-          primaryKey: true,
-        },
-        eventId: {
-          type: Sequelize.INTEGER,
-          primaryKey: true,
+  Schedule.init(
+    {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      start: Sequelize.DATE,
+      end: Sequelize.DATE,
+      locationLatitude: Sequelize.FLOAT,
+      locationLongitude: Sequelize.FLOAT,
+      eventId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: Event,
+          key: 'id',
         },
       },
-      {sequelize, modelName: 'UserParticipatedEvent'});
+      // TODO(arin-kwak): Find better way to deal with repetition.
+      //  Maybe we can reference calendar apps to improve this.
+      //  This method can't deal with 'evey other tuesday'
+      // If 'repeat' is null, this schedule is not repeated.
+      repeat: Sequelize.ENUM('week', 'month', 'year'),
+    }, {
+      sequelize,
+      modelName: 'schedule',
+      timestamps: false,
+    });
+
+  UserParticipatedEvent.init(
+    {
+      userId: {
+        type: Sequelize.INTEGER,
+        primaryKey: true
+      },
+      eventId: {
+        type: Sequelize.INTEGER,
+        primaryKey: true
+      }
+    },
+    { sequelize, modelName: "UserParticipatedEvent" }
+  );
 
   // Synchronize the models with the database
   // TODO(arin-kwak): In production phase, consider using migration instead of 'sync'.
@@ -145,5 +155,5 @@ const createStore = () => {
 };
 
 module.exports = {
-  createStore,
+  createStore
 };
