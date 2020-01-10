75b38126f0f23bb08eb367ed76af756a016ef47c
mskwon1 <mskwon@kookmin.ac.kr>
2020-01-09 22:00:47 -0800

    feat: install react-router-dom and add routes from App.js
    install react-router-dom
    add routes /, /signin, /signup
    include temporary style codes for the navigation bar
    add pages/signin.js blank page

--- web/src/App.js
+++ web/src/App.js
@@ -1,13 +1,46 @@
-import React from 'react';
-import Landing from './pages/landing';
+import React from 'react'
+import Events from './pages/events'
+import Signup from './pages/signup'
+import Signin from './pages/signin'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom'
 
 function App() {
+  // TODO : create a css stylesheet and remove these temporary styles
+  const signin_style = {
+    'float': 'right',
+  }
+  const title_style = {
+    'text-decoration': 'none',
+    'font-size': '35px',
+    'font-weight': 'bold'
+  }
+  // TODO : add a state to check if the user is logged in now
   return (
-      <div>
-        Main
-        <Landing/>
-      </div>
+    <Router>
+      {/* TODO : design navbar */}
+      <nav>
+        <Link to='/' style={title_style}>TinySocial</Link>
+        {/* TODO : set this to sign out when the user is logged in */}
+        <Link to="/signin" style={signin_style}>Sign In</Link>
+      </nav>
+      <Switch>
+        <Route exact path="/">
+          <Events />
+        </Route>
+        <Route path="/signin">
+          <Signin />
+        </Route>
+        <Route path="/signup">
+          <Signup />  
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
-export default App;
+export default App
---
+++ web/src/pages/signin.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+class Signin extends Component {
+  render() {
+    return (
+        <div>
+
+        </div>
+    );
+  }
+}
+
+Signin.propTypes = {};
+
+export default Signin;
